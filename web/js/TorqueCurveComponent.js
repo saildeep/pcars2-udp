757ea8db645babec41a3633e1a9bbc20e426182c
Jonas Körner <jkoerner@odd-it.com>
2018-01-04 02:09:08 +0100

    Added boost display with maybe not working unit conversion

--- web/js/TorqueCurveComponent.js
+++ web/js/TorqueCurveComponent.js
@@ -11,12 +11,16 @@ class TorqueCurveComponent extends BaseComponent{
     }
 
     OnReset(){
-        this.torque = new ApproximatedMaxCurve(100);
-        this.power = new ApproximatedMaxCurve(100);
+        const resolution = 300;
+
+        this.torque = new ApproximatedMaxCurve(resolution);
+        this.power = new ApproximatedMaxCurve(resolution);
+        this.boost = new ApproximatedMaxCurve(resolution);
         this.gears = new GearRatioTracker();
         this.opticalIntersections = 100;
         this.samplesTorque = this.svg.append('g');
         this.samplesPower = this.svg.append('g');
+        this.samplesBoost = this.svg.append('g');
         this.xAxisLines = this.svg.append('g');
         this.xAxisTexts = this.svg.append('g');
         this.currentRpmGroup = this.svg.append('g');
@@ -63,6 +67,12 @@ class TorqueCurveComponent extends BaseComponent{
         .attr('x1',this.width() * 0.1)
         .attr('x2',this.width()*0.9)
         .attr('stroke','blue');
+
+        this.maxBoostText = this.svg.append('text').attr('x',0.9 * this.width()).attr('fill','green');
+        this.maxBoostLine = this.svg.append('line')
+        .attr('x1',this.width() * 0.1)
+        .attr('x2',this.width()*0.9)
+        .attr('stroke','green');
        
         this.lastMaxX = 0;
     }
@@ -78,10 +88,11 @@ class TorqueCurveComponent extends BaseComponent{
         if(data.sRpm > this.torque.getMaxX()|| this.torque.getMaxX() < 1)
             return;
 
-        const xcoord1 = 0.1 * this.width() + 0.8 * this.width() * data.sRpm / this.torque.getMaxX();
-        const xcoord2 = 0.1 * this.width() + 0.8 * this.width() * Math.min(this.gears.rpmTarget(gear,data.sSpeed),this.torque.getMaxX() )/ this.torque.getMaxX();
-        this.currentRpmDisplay.attr('x1',xcoord1).attr('x2',xcoord2);
-        
+        if(gear > 0){
+            const xcoord1 = 0.1 * this.width() + 0.8 * this.width() * data.sRpm / this.torque.getMaxX();
+            const xcoord2 = 0.1 * this.width() + 0.8 * this.width() * Math.min(this.gears.rpmTarget(gear,data.sSpeed),this.torque.getMaxX() )/ this.torque.getMaxX();
+            this.currentRpmDisplay.attr('x1',xcoord1).attr('x2',xcoord2);
+        }
         //display for next and previous gear
         const nextRpm = this.gears.rpmNext(gear,data.sRpm);
         const xcoordNext = 0.1 * this.width() + 0.8 * this.width() * nextRpm / this.torque.getMaxX();
@@ -102,9 +113,11 @@ class TorqueCurveComponent extends BaseComponent{
        
         const t = data.sEngineTorque;
         const p = t * data.sRpm * (1/60) * 2 * Math.PI * 1e-3 * 1.32;
+        const b = (data.sTurboBoostPressure /100000) - 1; //convert to bar
         const pChanged = this.power.update(data.sRpm,p);
         const tChanged = this.torque.update(data.sRpm,t);
-        const curveDirty =  pChanged || tChanged;
+        const bChanged = this.boost.update(data.sRpm,b);
+        const curveDirty =  pChanged || tChanged ;
         const lastMaxXDirty = this.lastMaxX != this.power.getMaxX();
         
         
@@ -142,16 +155,33 @@ class TorqueCurveComponent extends BaseComponent{
             .attr('cy',(d,i)=>{return 0.1 * this.height() + d * 0.8 * this.height() / maxAxis });
 
 
+            this.samplesBoost
+            .selectAll('circle')
+            .data(this.boost.getIntersected(this.opticalIntersections))
+            .enter()
+            .append('circle')
+            .attr('r',5)
+            .attr('stroke','green')
+            .attr('fill','green');
 
+            this.samplesBoost.selectAll('circle')
+            .attr('cx',(d,i)=>{return 0.1 * this.width() + i  * this.width() * 0.8 / this.opticalIntersections })
+            .attr('cy',(d,i)=>{return 0.1 * this.height() + d * 0.8 * this.height() * 0.75 / this.boost.getMax() });
+
+        
             const torqueY = 0.1* this.height() + this.height() * this.torque.getMax()/maxAxis * 0.8;
             
-                    this.maxTorqueText.attr('y',torqueY).html(this.torque.getMax().toFixed(0) + "NM");
-                    this.maxTorqueLine.attr('y1',torqueY).attr('y2',torqueY);
-            
-            
-                    const powerY = 0.1 * this.height() + this.height() * this.power.getMax() / maxAxis * 0.8;
-                    this.maxPowerText.attr('y',powerY).html(this.power.getMax().toFixed(0)+ "HP");
-                    this.maxPowerLine.attr('y1',powerY).attr('y2',powerY);
+            this.maxTorqueText.attr('y',torqueY).html(this.torque.getMax().toFixed(0) + "NM");
+            this.maxTorqueLine.attr('y1',torqueY).attr('y2',torqueY);
+    
+    
+            const powerY = 0.1 * this.height() + this.height() * this.power.getMax() / maxAxis * 0.8;
+            this.maxPowerText.attr('y',powerY).html(this.power.getMax().toFixed(0)+ "HP");
+            this.maxPowerLine.attr('y1',powerY).attr('y2',powerY);
+
+            const boostY = this.boost.getMax() > 0.01 ? this.height() * 0.1 + this.height() * 0.8 * 0.75 : this.height() * 0.1;
+            this.maxBoostText.attr('y',boostY).html(this.boost.getMax().toFixed(2) + "Bar");
+            this.maxBoostLine.attr('y1',boostY).attr('y2',boostY);
                     
         }
         
